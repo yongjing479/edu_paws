1c55fbe45d993ca3ab8e8e2cde914bf04e64d3e0
yongjing479 <122367568+yongjing479@users.noreply.github.com>
2023-10-17 18:24:46 +0800

    animated number is visible  without scrolling down

--- edupaws/app/Profile/performanceSummary/animatedNumber.tsx
+++ edupaws/app/Profile/performanceSummary/animatedNumber.tsx
@@ -5,12 +5,12 @@ import { Col, Row, Statistic } from 'antd';
 const formatter = (value:number) => <CountUp end={value} separator="," />;
 
 const App: React.FC = () => (
-    <div className='p-4 mt-5 text-center'>
+    <div className='p-2 text-center'>
         <Row gutter={8}>
             <Col span={8}>
             <Statistic
                 title={
-                <span className="font-lg text-xl text-gray-800">Quiz (min)</span>
+                <span className="font-medium text-xl text-gray-800">Quiz (min)</span>
                 }
                 value={10}
                 formatter={formatter}
@@ -19,7 +19,7 @@ const App: React.FC = () => (
             <Col span={8}>
             <Statistic
                 title={
-                <span className="font-lg text-xl text-gray-800">Average Scores (%)</span>
+                <span className="font-medium text-xl text-gray-800">Average Scores (%)</span>
                 }
                 value={100}
                 precision={2}
@@ -29,7 +29,7 @@ const App: React.FC = () => (
             <Col span={8}>
             <Statistic
                 title={
-                <span className="font-lg text-xl text-gray-800">Total Study Hour (hr)</span>
+                <span className="font-medium text-xl text-gray-800">Total Study Hour (hr)</span>
                 }
                 value={20}
                 precision={2}
