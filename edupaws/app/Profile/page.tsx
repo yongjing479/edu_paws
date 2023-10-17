5fc9b947da9eb1ea2dfd4e9da0e1aed85a0b6270
yongjing479 <122367568+yongjing479@users.noreply.github.com>
2023-10-17 18:25:58 +0800

    changed file name to summaryTab

--- edupaws/app/Profile/page.tsx
+++ edupaws/app/Profile/page.tsx
@@ -2,14 +2,14 @@
 import React from "react";
 import NavBar from "../homeComponent/navBar";
 import ProfileContainer from "./profileContainer/profileContainer";
-import SummaryContainer from "./performanceSummary/summaryTab";
+import SummaryTab from "./performanceSummary/summaryTab";
 
 function page() {
   return (
     <div>
       <NavBar />
       <ProfileContainer />
-      <SummaryContainer />
+      <SummaryTab />
     </div>
   );
 }
